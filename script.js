a177eb1649bd44a7593f045ac023c9a58bf0c1b1
izik4004 <izik4004@gmail.com>
2022-05-12 20:02:43 +0100

    changes

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const signUp = e => {
     else{
         alert("Ooopppssss... Duplicate found!!!\nYou have already sigjned up");
     }
+   
     e.preventDefault();
 }
 
@@ -28,17 +29,47 @@ function signIn(e) {
     let email = document.getElementById('username').value;
     let formData = JSON.parse(localStorage.getItem('formData')) || [];
     let exist = formData.length && 
-    JSON.parse(localStorage.getItem('formData')).some(data => data.username.toLowerCase() == email);
+    JSON.parse(localStorage.getItem('formData')).some(data => data.username.toLowerCase() || data.username.toUpperCase() == email);
     if(!exist){
         alert("Incorrect login credentials");
     }
     else{
-        location.href = "/";
+        location.href = "/home.html";
     }
+    
+    // storage();
     e.preventDefault();
+    // console.log(JSON.parse(localStorage.setItem(formData)))
 }
 
-function welcome(e){
-    let username = document.getElementById('username').value;
 
+// function storage() {
+
+//    let {username} =  JSON.parse(localStorage.getItem('formData')).some(data => data.username);
+//    var output = document.getElementById('output')
+//    output.innerHTML = `
+//    <div>Welcome</div>
+//    <div>${username}</div>
+//    `
+
+// }
+
+function get() {
+    
+    var storedValue = JSON.parse(localStorage.getItem('formData')) || [];
+//    console.log(storedValue.username)
+    if (storedValue) {
+        document.getElementById('output').innerHTML = localStorage.getItem("username");
+    }
+}
+
+
+
+for (i = 0; i < localStorage.length; i++)   {
+    console.log(localStorage.key(i) + "=[" + localStorage.getItem(localStorage.key(i)) + "]");
+}
+
+console.log("session storage");
+for (i = 0; i < sessionStorage.length; i++) {
+    console.log(sessionStorage.key(i) + "=[" + sessionStorage.getItem(sessionStorage.key(i)) + "]");
 }
